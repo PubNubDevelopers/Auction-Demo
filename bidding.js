dffd211da3bf63adf503f2903bc72cefc89e9e22
Chandler Mayo <chandler@chandlermayo.com>
2022-09-29 15:38:52 -0400

    Fixing actionCompleted

--- bidding.js
+++ bidding.js
@@ -4,105 +4,7 @@ let pubnub = null;
 let first_result = null;
 let current_channel = null;
 let itemsArea = null;
-
-//The below function is for the interactive demo on PubNub.com. It is not part of the demo of this application.
-console.log(window.location.href); 
-var actionCompleted = function(args) {
-    console.log("action completed");
-    const pub = 'pub-c-c8d024f7-d239-47c3-9a7b-002f346c1849';
-    const sub = 'sub-c-95fe09e0-64bb-4087-ab39-7b14659aab47';
-    let identifier = "";
-    let action = args.action;
-    let blockDuplicateCalls = args.blockDuplicateCalls;
-    let debug = args.debug;
-    let windowLocation = args.windowLocation;
-
-    if (typeof action === 'undefined')
-    {
-        console.log('Interactive Demo Integration Error: No action provided');        
-        return;
-    }
-
-    if (typeof blockDuplicateCalls === 'undefined')
-    {
-        blockDuplicateCalls = true;
-    }
-
-    if (typeof debug === 'undefined')
-    {
-        debug = false;
-    }
-
-    //  If invoked from client-side, you can omit the window location
-    if (typeof windowLocation === 'undefined')
-        windowLocation = window.location.href;
-
-    var fetchClient = null;
-    if (typeof fetch === 'undefined')
-        fetchClient = args.fetchClient;
-    else
-        fetchClient = fetch;
-
-    let queryString = new URL(windowLocation).search.substring(1);
-    const urlParamsArray = queryString.split('&');
-    for (let i = 0; i < urlParamsArray.length; i++) {
-        if (urlParamsArray[i].startsWith('identifier') && urlParamsArray[i].includes('=')) {
-            let identifierPair = urlParamsArray[i].split('=');
-            identifier = identifierPair[1];
-        }
-    }
-    if (identifier === "") {
-        if (debug)
-        {
-            console.log('Interactive Demo Integration Error: Failed to detect identifier in URL query string');
-        }
-        return;
-    }
-    if (blockDuplicateCalls) {
-        //  This option only works if the sessionStorage object is defined (client-side only)
-        try {
-            if (!(typeof sessionStorage === 'undefined')) {
-                //  Read the id from session storage and only send the message if the message was not previous sent
-                let sessionStorageKey = "Demo_" + identifier + action;
-                let storedId = sessionStorage.getItem(sessionStorageKey);
-                if (storedId == null) {
-                    if (debug)
-                        console.log('Setting session key to avoid duplicate future messages being sent. Action: ' + action + '. Identifier: ' + identifier);
-                        sessionStorage.setItem(sessionStorageKey, "set");
-                }
-                else {
-                    //  This is a duplicate message, do not send it
-                    if (debug)
-                        console.log('Message blocked as it is a duplicate. Action: ' + action + '. Identifier: ' + identifier);
-                    return;
-                }
-            }                   
-        }
-        catch (err) {} //  Session storage is not available
-    }
-
-    if (debug)
-    {
-        console.log('Sending message. Action: ' + action + '. Identifier: ' + identifier);
-    }
-    
-    const url = `https://ps.pndsn.com/publish/${pub}/${sub}/0/demo/myCallback/${encodeURIComponent(JSON.stringify({ id: identifier, feature: action }))}?store=0&uuid=${identifier}`;
-    fetchClient(url)
-        .then(response => {
-        if (!response.ok) {
-            throw new Error(response.status + ' ' + response.statusText);
-        }
-        return response;
-    })
-        .then(data => {
-        //  Successfully set demo action with demo server
-        console.log("Guided Demo Integration success", url, data)
-    })
-        .catch(e => {
-        console.log('Interactive Demo Integration: ', e);
-    });
-    return;
-} 
+let orgwindowlocation = sessionStorage['orgwindowlocation'];
 
 window.onload = async () => {
     await configureClient()
@@ -342,7 +244,7 @@ function placeBid(new_msg_bidding_channel, new_msg_name, new_msg_description, ne
                             // handle status, response
                             // console.log(response);
                         });
-                        actionCompleted({action: 'Place A Bid'}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
+                        actionCompleted({action: 'Place A Bid', windowLocation: orgwindowlocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
                         alert("Your bid has been placed.")
                     } else {
                         alert("Your new bid must be higher than the previous bids.")
@@ -375,6 +277,9 @@ function loadMore() {
                     auction_item.displayItem(pubnub.getUUID(), message.uuid, message, message.timetoken);
                 });
             }
+            if (document.getElementById('items-area').childNodes.length === 0) {
+                document.getElementById('items-area').innerHTML = "Nothing here to show. Try creating a new auction or placing a bid."
+            } 
         }
     );
     return false;
@@ -438,7 +343,7 @@ function newItem() {
         // handle status, response
         //console.log(response);
     });
-    actionCompleted({action: 'Create An Auction'}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
+    actionCompleted({action: 'Create An Auction', windowLocation: orgwindowlocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
 
     alert("New auction has been posted.");
     document.getElementById('contactInput').value = "";
@@ -460,14 +365,14 @@ function loadBidding(channel) {
         document.getElementById('online-users-label').style.visibility = 'hidden';
     } else if (channel == "auctions") { // If loading your bids or auctions then hide the create new auction button
         setActive("auctions");
-        actionCompleted({action: 'View Your Auctions'}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
+        actionCompleted({action: 'View Your Auctions', windowLocation: orgwindowlocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
         current_channel = sub + "_auctions";
         document.getElementById('cat-label-head').innerHTML = "Your Auctions";
         document.getElementById('create_new').style.visibility = 'hidden';
         document.getElementById('online-users-label').style.visibility = 'hidden';
     } else {
         setActive(channel);
-        actionCompleted({action: 'Change Category'}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
+        actionCompleted({action: 'Change Category', windowLocation: orgwindowlocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
         document.getElementById('cat-label-head').innerHTML = channel;
         document.getElementById('create_new').style.visibility = 'visible';
         document.getElementById('online-users-label').style.visibility = 'visible';
@@ -523,9 +428,6 @@ function loadBidding(channel) {
                     auction_item.displayItem(pubnub.getUUID(), message.uuid, message.message, message.timetoken);
                 });
             }
-            if (document.getElementById('items-area').childNodes.length === 0) {
-                document.getElementById('items-area').innerHTML = "<h2>Nothing here to show. Try creating a new auction or placing a bid.</h2>"
-            } 
         }
     );
 }
@@ -544,8 +446,104 @@ const updateUI = async () => {
         });
         sub = userdetails.sub;
         loadBidding("your-bids");
-        actionCompleted({action: 'Login To Bid'}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
+        actionCompleted({action: 'Login To Bid', windowLocation: orgwindowlocation}); // This is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
         document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
     }
 }
 
+//The below function is for the interactive demo on PubNub.com. It is not part of the demo of this application. 
+var actionCompleted = function(args) {
+    const pub = 'pub-c-c8d024f7-d239-47c3-9a7b-002f346c1849';
+    const sub = 'sub-c-95fe09e0-64bb-4087-ab39-7b14659aab47';
+    let identifier = "";
+    let action = args.action;
+    let blockDuplicateCalls = args.blockDuplicateCalls;
+    let debug = args.debug;
+    let windowLocation = args.windowLocation;
+
+    if (typeof action === 'undefined')
+    {
+        console.log('Interactive Demo Integration Error: No action provided');        
+        return;
+    }
+
+    if (typeof blockDuplicateCalls === 'undefined')
+    {
+        blockDuplicateCalls = true;
+    }
+
+    if (typeof debug === 'undefined')
+    {
+        debug = false;
+    }
+
+    //  If invoked from client-side, you can omit the window location
+    if (typeof windowLocation === 'undefined')
+        windowLocation = window.location.href;
+
+    var fetchClient = null;
+    if (typeof fetch === 'undefined')
+        fetchClient = args.fetchClient;
+    else
+        fetchClient = fetch;
+
+    let queryString = new URL(windowLocation).search.substring(1);
+    const urlParamsArray = queryString.split('&');
+    for (let i = 0; i < urlParamsArray.length; i++) {
+        if (urlParamsArray[i].startsWith('identifier') && urlParamsArray[i].includes('=')) {
+            let identifierPair = urlParamsArray[i].split('=');
+            identifier = identifierPair[1];
+        }
+    }
+    if (identifier === "") {
+        if (debug)
+        {
+            console.log('Interactive Demo Integration Error: Failed to detect identifier in URL query string');
+        }
+        return;
+    }
+    if (blockDuplicateCalls) {
+        //  This option only works if the sessionStorage object is defined (client-side only)
+        try {
+            if (!(typeof sessionStorage === 'undefined')) {
+                //  Read the id from session storage and only send the message if the message was not previous sent
+                let sessionStorageKey = "Demo_" + identifier + action;
+                let storedId = sessionStorage.getItem(sessionStorageKey);
+                if (storedId == null) {
+                    if (debug)
+                        console.log('Setting session key to avoid duplicate future messages being sent. Action: ' + action + '. Identifier: ' + identifier);
+                        sessionStorage.setItem(sessionStorageKey, "set");
+                }
+                else {
+                    //  This is a duplicate message, do not send it
+                    if (debug)
+                        console.log('Message blocked as it is a duplicate. Action: ' + action + '. Identifier: ' + identifier);
+                    return;
+                }
+            }                   
+        }
+        catch (err) {} //  Session storage is not available
+    }
+
+    if (debug)
+    {
+        console.log('Sending message. Action: ' + action + '. Identifier: ' + identifier);
+    }
+    
+    const url = `https://ps.pndsn.com/publish/${pub}/${sub}/0/demo/myCallback/${encodeURIComponent(JSON.stringify({ id: identifier, feature: action }))}?store=0&uuid=${identifier}`;
+    fetchClient(url)
+        .then(response => {
+        if (!response.ok) {
+            throw new Error(response.status + ' ' + response.statusText);
+        }
+        return response;
+    })
+        .then(data => {
+        //  Successfully set demo action with demo server
+        console.log("Guided Demo Integration success", url, data)
+    })
+        .catch(e => {
+        console.log('Interactive Demo Integration: ', e);
+    });
+    return;
+} 
